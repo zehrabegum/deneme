1251c9d7b642dd98d0da02d5dc0a56a08a813661
zehrabegum <62847967+zehrabegum@users.noreply.github.com>
2020-04-03 00:19:38 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İKİ TANE SAYİ GİRİNİZ:");
+            int sayi1 = int.Parse(Console.ReadLine());
+            int sayi2 = int.Parse(Console.ReadLine());
+            int buyuk;
+            int kucuk;
+            if (sayi1 < sayi2)
+            {
+                buyuk = sayi2;
+                kucuk = sayi1;
+            }
+            else
+            {
+                buyuk = sayi1;
+                kucuk = sayi2;
+            }
+            int kat = kucuk;
+            while (kat % kucuk != 0 || kat % buyuk != 0)
+            {
+                kat=kat+kucuk;
+            }
+            Console.WriteLine("sonuc:{0}", kat);
+        }
+    }
+    
+}
